0140f79e9e5df3687b110a1cd38c5c898aa47ae8
Dean Wagner <dean@dfwagners.net>
2021-03-08 11:58:53 -0500

    boilerplate for Three.js

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,4 +1,5 @@
 import * as THREE from '/build/three.module.js'
+import { OrbitControls } from '/jsm/controls/OrbitControls'
 
 const scene: THREE.Scene = new THREE.Scene()
 
@@ -8,13 +9,15 @@ const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+const controls = new OrbitControls( camera, renderer.domElement )
+
 const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
 
 const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-camera.position.z = 2
+camera.position.z = 10
 
 var animate = function () {
 
@@ -22,6 +25,8 @@ var animate = function () {
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    controls.update()
 
     renderer.render(scene, camera)
 
